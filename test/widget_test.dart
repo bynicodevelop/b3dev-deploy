cfe8f6969033f4b3bf9bc34298d456b078ef56f3
nico-develop <contact@nico-develop.fr>
2022-01-04 14:12:41 +0100

    Fixed tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -26,7 +26,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-
-    expect(0, 1);
   });
 }
